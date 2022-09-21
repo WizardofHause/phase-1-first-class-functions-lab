06c401fa65366644c8a7bb209602bc36ac347e47
Jon Hause <jonrhause@gmail.com>
2022-09-21 15:38:56 -0600

    commit tree eff31b4fa12bbca981dfd7dbf1338127d87ee100

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const scuberDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (scuberDrivers) {
+    return scuberDrivers.slice(0, 2);
+}
+const returnLastTwoDrivers = function (scuberDrivers) {
+    return scuberDrivers.slice(2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplier) {
+    return function (value) {
+        return multiplier * value;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (scuberDrivers, value) {
+    return value(scuberDrivers); 
+}
